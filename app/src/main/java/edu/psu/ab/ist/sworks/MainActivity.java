0a55f7e6678ce9b6004d5ecb39db4940e1d3b663
joeoakes <joe.oakes@gmail.com>
2019-04-08 16:02:07 -0400

    Header Comments

--- app/src/main/java/edu/psu/ab/ist/sworks/MainActivity.java
+++ app/src/main/java/edu/psu/ab/ist/sworks/MainActivity.java
@@ -1,3 +1,12 @@
+/*
+Project: PSA Grover Vehicle Sworks edition
+Feature: Main Android Activity
+Course: IST 440w Section 1 Fall 2019
+Author: Joe Oakes
+Date Developed: 4/8/2019
+Last Date Changed:
+Rev: development build 1
+*/
 package edu.psu.ab.ist.sworks;
 
 import android.content.Intent;
--- app/src/main/java/edu/psu/ab/ist/sworks/MissionStatus.java
+++ app/src/main/java/edu/psu/ab/ist/sworks/MissionStatus.java
@@ -1,3 +1,13 @@
+/*
+Project: PSA Grover Vehicle Sworks edition
+Feature: Mission Status to handle go or no go mission situtation
+Course: IST 440w Section 1 Fall 2019
+Author: Joe Oakes
+Date Developed: 4/8/2019
+Last Date Changed:
+Rev: development build 1
+*/
+
 package edu.psu.ab.ist.sworks;
 
 import android.bluetooth.BluetoothAdapter;
